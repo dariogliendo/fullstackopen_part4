634370aebc760cec8c210ff059ba8dee0f284cda
dliendo <dario.liendo@gmail.com>
2023-11-25 21:14:06 -0300

    login

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,51 +2,50 @@ const blogsRoute = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
-blogsRoute.get('/', async (request, response) => {
+blogsRoute.get('/', async (request, response, next) => {
   try {
-    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
     response.json(blogs);
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
 
 blogsRoute.post('/', async (request, response, next) => {
   try {
-    console.log(request.body)
-    if (!request.body.title) return response.status(400).json({ error: 'title missing' })
-    if (!request.body.url) return response.status(400).json({ error: 'url missing' })
+    if (!request.body.title) return response.status(400).json({ error: 'title missing' });
+    if (!request.body.url) return response.status(400).json({ error: 'url missing' });
     const users = await User.find({});
     const blog = new Blog(request.body);
     const authorUser = users[Math.floor(Math.random() * users.length)];
 
     blog.user = authorUser._id;
 
-    const result = await blog.save()
+    const result = await blog.save();
     authorUser.blogs = [...authorUser.blogs, result._id];
-    await authorUser.save()
-    response.status(201).json(result)
+    await authorUser.save();
+    response.status(201).json(result);
   } catch (error) {
-    next(error)
+    next(error);
   }
 });
 
 blogsRoute.delete('/:id', async (request, response, next) => {
   try {
-    await Blog.findByIdAndRemove(request.params.id)
-    response.status(204).end()
+    await Blog.findByIdAndRemove(request.params.id);
+    response.status(204).end();
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 blogsRoute.put('/:id', async (request, response, next) => {
   try {
-    const result = await Blog.findByIdAndUpdate(request.params.id, request.body, { new: true })
-    response.json(result)
+    const result = await Blog.findByIdAndUpdate(request.params.id, request.body, { new: true });
+    response.json(result);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 module.exports = blogsRoute;
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const logger = require('./utils/logger');
 const config = require('./utils/config');
 const middleware = require('./utils/middleware');
@@ -17,6 +18,7 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
 
